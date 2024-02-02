2efebdcf5f4481a0a7dc107c1d95ff6f059772a1
Vladimir Arger <8745@rambler.ru>
2024-02-02 17:16:52 +0400

    Doing homework 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+      console.log("массивы разные по длине");
+      return false;
+    } else if (arr1.every((item, index) => item === arr2[index])) {
+      console.log("массивы одинаковые");
+      return true;
+    } else {
+      console.log("у массивов разное содержимое");
+      return false;
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const usersFilter = users.filter((user) => user.gender === gender);
+    if (usersFilter.length === 0) return 0;
+    const result =
+      usersFilter.reduce((acc, user) => acc + user.age, 0) /
+      users.filter((user) => user.gender === gender).length;
+    return result;
 }
